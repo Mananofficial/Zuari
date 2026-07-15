2c662a6d570495636613439b1bb84f0322ff793a
Manan Sharma <manansharma.mail@gmail.com>
2026-07-15 13:57:37 +0530

    deploy vercel

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
